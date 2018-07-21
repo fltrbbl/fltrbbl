9c4b0fd50ae11765e102f2c459bafed53a5f68a4
jan <jan@kwoh.de>
2018-07-21 17:54:06 +0200

    add gunicorn, configureable host in dummy client

--- dummy_client/client.py
+++ dummy_client/client.py
@@ -5,9 +5,6 @@ import requests
 import os
 from requests.auth import HTTPBasicAuth
 
-HOST = 'http://localhost:8080'
-
-
 def resolve_json(path_or_json: str):
     if path_or_json.startswith('@'):
         path = path_or_json[1:]
@@ -23,24 +20,20 @@ def resolve_json(path_or_json: str):
 
 
 @click.group()
-def cli():
-    pass
+@click.option('--host', default='http://localhost:8080')
+@click.pass_context
+def cli(ctx, host):
+    ctx.obj['HOST'] = host
 
-
-@click.command()
-def dummyuser():
-    d = dict(
-        email='test@abc.de',
-        password='password'
-    )
 
 auth = dict(username="testemail@abc.de", password='testpassword')
 
 
 @cli.command()
 @click.argument('resource')
-def get(resource):
-    response = requests.get(f'{HOST}/{resource}', auth=HTTPBasicAuth(auth['username'], auth['password']))
+@click.pass_context
+def get(ctx, resource):
+    response = requests.get(f'{ctx.obj["HOST"]}/{resource}', auth=HTTPBasicAuth(auth['username'], auth['password']))
     try:
         print('response %s: %s' % (response.status_code, json.dumps(response.json(), indent=2, ensure_ascii=False)))
     except (ValueError, json.decoder.JSONDecodeError):
@@ -50,7 +43,8 @@ def get(resource):
 @cli.command()
 @click.argument('resource')
 @click.option('--json', '-d', 'json_dict')
-def post(resource, json_dict=''):
+@click.pass_context
+def post(ctx, resource, json_dict=''):
     if json_dict:
         d = resolve_json(json_dict)
     else:
@@ -60,7 +54,7 @@ def post(resource, json_dict=''):
         else:
             print('no input found..?')
             exit(1)
-    response = requests.post(f'{HOST}/{resource}', json=d, auth=HTTPBasicAuth(auth['username'], auth['password']))
+    response = requests.post(f'{ctx.obj["HOST"]}/{resource}', json=d, auth=HTTPBasicAuth(auth['username'], auth['password']))
     try:
         print('response %s: %s' % (response.status_code, json.dumps(response.json(), indent=2, ensure_ascii=False)))
     except (ValueError, json.decoder.JSONDecodeError):
@@ -69,7 +63,8 @@ def post(resource, json_dict=''):
 @cli.command()
 @click.argument('resource')
 @click.option('--json', '-d', 'json_dict')
-def put(resource, json_dict=''):
+@click.pass_context
+def put(ctx, resource, json_dict=''):
     if json_dict:
         d = resolve_json(json_dict)
     else:
@@ -79,7 +74,7 @@ def put(resource, json_dict=''):
         else:
             print('no input found..?')
             exit(1)
-    response = requests.put(f'{HOST}/{resource}', json=d, auth=HTTPBasicAuth(auth['username'], auth['password']))
+    response = requests.put(f'{ctx.obj["HOST"]}/{resource}', json=d, auth=HTTPBasicAuth(auth['username'], auth['password']))
     try:
         print('response %s: %s' % (response.status_code, json.dumps(response.json(), indent=2, ensure_ascii=False)))
     except (ValueError, json.decoder.JSONDecodeError):
@@ -88,7 +83,8 @@ def put(resource, json_dict=''):
 @cli.command()
 @click.argument('resource')
 @click.option('--json', '-d', 'json_dict')
-def delete(resource, json_dict=''):
+@click.pass_context
+def delete(ctx, resource, json_dict=''):
     if json_dict:
         d = resolve_json(json_dict)
     else:
@@ -98,11 +94,11 @@ def delete(resource, json_dict=''):
         else:
             print('no input found..?')
             exit(1)
-    response = requests.delete(f'{HOST}/{resource}', json=d, auth=HTTPBasicAuth(auth['username'], auth['password']))
+    response = requests.delete(f'{ctx.obj["HOST"]}/{resource}', json=d, auth=HTTPBasicAuth(auth['username'], auth['password']))
     try:
         print('response %s: %s' % (response.status_code, json.dumps(response.json(), indent=2, ensure_ascii=False)))
     except (ValueError, json.decoder.JSONDecodeError):
         print('response %s: %s' % (response.status_code, response.text))
 
 if __name__ == '__main__':
-    cli()
+    cli(obj={})
